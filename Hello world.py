e6bfa74adb66b2bd962517cea7ff3d0bdb1482b5
mahesh123456769 <weirdsoulmahesh@gmail.com>
2024-09-20 13:59:57 +0530

    Add files via upload

---
+++ Hello world.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def hello():
+    """print "Hello world" and return None."""
+    print("Hello world")
+hello()
+     
